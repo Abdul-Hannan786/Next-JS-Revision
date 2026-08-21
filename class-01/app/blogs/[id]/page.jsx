4f7aa06b76d01a101365af711e958d0f0ae1110d
Abdul Hannan <abdulhannanhere@gmail.com>
2026-08-21 08:00:39 +0500

    not-found page review

--- class-01/app/blogs/[id]/page.jsx
+++ class-01/app/blogs/[id]/page.jsx
@@ -1,8 +1,13 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 const BlogDetailsPage = async ({ params }) => {
   const { id } = await params;
   console.log(await params);
+
+  if(!/^\d+$/.test(id)){
+    notFound()
+  }
   
   return <div>BlogDetailsPage {id}</div>;
 };
